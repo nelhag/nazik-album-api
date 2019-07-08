104a5ba713808647e7c0db97900c86003676837f
nelhag <lamais2006@yahoo.com>
2019-07-08 15:43:34 -0400

    created nazik-album project

---
+++ src/main/java/wcci/nazikalbumapi/NazikAlbumApiApplication.java
@@ -0,0 +1,13 @@
+package wcci.nazikalbumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NazikAlbumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NazikAlbumApiApplication.class, args);
+	}
+
+}
